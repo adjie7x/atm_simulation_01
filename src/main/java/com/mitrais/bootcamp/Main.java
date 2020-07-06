500b5c78d7f3423f2ac1ee16bac4a8aea700593e
aji_mulyadi <adjie.mulyadi7x@gmail.com>
2020-07-06 11:16:18 +0700

    init

---
+++ src/main/java/com/mitrais/bootcamp/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2020 All Rights Reserved.
+ */
+package com.mitrais.bootcamp;
+
+/**
+ * @author Aji Atin Mulyadi
+ * @version $Id: Main.java, v 0.1 2020‐07‐06 10:17 Aji Atin Mulyadi Exp $$
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
